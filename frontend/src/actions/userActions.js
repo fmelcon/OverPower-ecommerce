4e64cb645b6a89668ff89e38a571978ffffe44b7
Franco Melcon <fmelcon@live.com>
2021-06-15 19:03:41 -0300

    add Register Screen funtionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -4,6 +4,9 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCES,
   USER_LOGOUT,
+  USER_REGISTER_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCES,
 } from "../constants/userConstants";
 
 export const login = (email, password) => async (dispatch) => {
@@ -42,4 +45,41 @@ export const login = (email, password) => async (dispatch) => {
 export const logout = () => (dispatch) => {
   localStorage.removeItem("userInfo");
   dispatch({ type: USER_LOGOUT });
+};
+
+export const register = (name, email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_REGISTER_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/",
+      { name, email, password },
+      config
+    );
+    dispatch({
+      type: USER_REGISTER_SUCCES,
+      payload: data,
+    });
+    dispatch({
+      type: USER_LOGIN_SUCCES,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_REGISTER_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
 };
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -3,6 +3,9 @@ import {
   USER_LOGIN_SUCCES,
   USER_LOGOUT,
   USER_LOGIN_FAIL,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCES,
+  USER_REGISTER_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -12,6 +15,21 @@ export const userLoginReducer = (state = {}, action) => {
     case USER_LOGIN_SUCCES:
       return { loading: false, userInfo: action.payload };
     case USER_LOGIN_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { loading: true };
+    case USER_REGISTER_SUCCES:
+      return { loading: false, userInfo: action.payload };
+    case USER_REGISTER_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
