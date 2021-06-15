1838dd2191543eda8de95bad8518e8959be451d0
Franco Melcon <fmelcon@live.com>
2021-06-15 17:09:05 -0300

    add user reducer action and constant

---
+++ frontend/src/actions/userActions.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import {
+  USER_LOGIN_FAIL,
+  USER_LOGIN_REQUEST,
+  USER_LOGIN_SUCCES,
+} from "../constants/userConstants";
+
+export const login = (email, password) => async (dispatch) => {
+  try {
+    dispatch({
+      type: USER_LOGIN_REQUEST,
+    });
+
+    const config = {
+      headers: {
+        "content-type": "application/json",
+      },
+    };
+
+    const { data } = await axios.post(
+      "/api/users/login",
+      { email, password },
+      config
+    );
+    dispatch({
+      type: USER_LOGIN_SUCCES,
+      payload: data,
+    });
+    localStorage.setItem("userInfo", JSON.stringify(data));
+  } catch (error) {
+    dispatch({
+      type: USER_LOGIN_FAIL,
+      payload:
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message,
+    });
+  }
+};
