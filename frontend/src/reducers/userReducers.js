d4d2ab6c7a9343b428a4d1a46db7c74c4338ac0c
Franco Melcon <fmelcon@live.com>
2021-06-16 11:00:14 -0300

    userProfile funtionality

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
   USER_REGISTER_REQUEST,
   USER_REGISTER_SUCCES,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCES,
+  USER_DETAILS_FAIL,
 } from "../constants/userConstants";
 
 export const userLoginReducer = (state = {}, action) => {
@@ -30,6 +33,21 @@ export const userRegisterReducer = (state = {}, action) => {
     case USER_REGISTER_SUCCES:
       return { loading: false, userInfo: action.payload };
     case USER_REGISTER_FAIL:
+      return { loading: false, error: action.payload };
+    case USER_LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export const userDetailsReducer = (state = {}, action) => {
+  switch (action.type) {
+    case USER_DETAILS_REQUEST:
+      return { ...state, loading: true };
+    case USER_DETAILS_SUCCES:
+      return { loading: false, user: action.payload };
+    case USER_DETAILS_FAIL:
       return { loading: false, error: action.payload };
     case USER_LOGOUT:
       return {};
