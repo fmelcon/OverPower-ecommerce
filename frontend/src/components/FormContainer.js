2ee6114beabf348efcdd9fea304c5763d56ba8cb
Franco Melcon <fmelcon@live.com>
2021-06-16 17:16:15 -0300

    PaymentScreen funtionality

--- frontend/src/components/FormContainer.js
+++ frontend/src/components/FormContainer.js
@@ -5,7 +5,7 @@ const FormContainer = ({ children }) => {
   return (
     <Container>
       <Row className="justify-content-md-center">
-        <Col xs={16} md={8} lg={8}>
+        <Col xs={12} md={8}>
           {children}
         </Col>
       </Row>
